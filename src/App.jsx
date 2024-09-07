eb82ce14eb17653e2f1c3e62dcd3b07bf98d607a
Eugenia Sansierra <eugesans@gmail.com>
2024-09-07 15:06:07 -0300

    se modifco carpetas,se agrego npm bootstrap y se modifico parte principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   
 
   return (
-    <h1>hola mundo</h1>
-     
+    <main className='container my-4' >  
+      <h1 className='text-center'>Primer Proyecto React</h1>
+    <p>Lorem  ipsum dolor sit amet consectetur adipisicing elit. <br/>Ex eius cum velit perspiciatis assumenda dicta veritatis libero numquam, vero officia expedita voluptate totam blanditiis reiciendis consequatur enim earum nemo quo!</p>
+      </main>
+  
+
   )
 }
 
